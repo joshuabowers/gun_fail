7bbb3281515702f70206ef2c077d3e577e8247e0
Joshua Bowers <joshua.bowers+code@gmail.com>
2013-05-28 00:06:57 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GunFail::Application.config.secret_token = 'b341c6cea467456880ba3ef92e7c378bee6a7a5b3f841f495187b449f364356fb3a8683358e06d9612cc5dc97a24c53002e37cb12c90276531502e160bdd8242'
